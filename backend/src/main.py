7f663a9890d110fe40f92cd4f4f6df354b20eb29
Dylan <dsja612@gmail.com>
2022-07-08 13:36:58 +0800

    Removed excess API endpoints

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,14 +1,12 @@
 from fastapi import FastAPI
 import query
+from fastapi import HTTPException
 
 app = FastAPI()
 
-@app.get("/users/basics/{username}")
-async def basics(username: str):
-    reddit = query.connectToReddit()
-    return query.getBasics(reddit, username)
-
-@app.get("/users/advanced/{username}")
-async def advanced(username: str):
-    reddit = query.connectToReddit()
-    return query.getTextStatistics(query.getComments(reddit, username))
+@app.get("/users/{username}")
+def main(username: str):
+    #try:
+    return query.main(username)
+    #except:
+        #raise HTTPException(status_code=404, detail="User doesn't exist!")
