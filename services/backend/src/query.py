4802261ef313f52038e45558e3fa1745247b5506
Dylan <dsja612@gmail.com>
2022-07-17 21:07:55 +0800

    Use asyncpraw instead of praw

--- services/backend/src/query.py
+++ services/backend/src/query.py
@@ -7,6 +7,7 @@ from string import punctuation
 
 from decouple import config
 import json
+import asyncpraw
 from . import api
 from . import preprocess
 import praw
@@ -15,7 +16,7 @@ from fastapi import HTTPException
 
 async def main(username: str): 
 
-    reddit = praw.Reddit(client_id = config('CLIENT_ID'),
+    reddit = asyncpraw.Reddit(client_id = config('CLIENT_ID'),
                      client_secret = config('CLIENT_SECRET'),
                      user_agent = config('USER_AGENT'))
 
@@ -32,17 +33,19 @@ async def main(username: str):
         payload['num_submissions'] = await api.getNumSubmissions(username, 'submission')
 
         ### PRAW
-        comments = reddit.redditor(username).comments.new(limit=300)
+        user = await reddit.redditor(username)
+        comments = user.comments.new(limit=500)
 
         # Get body of comments
         comment_list = []
         top_subreddits = {}
-        for comment in comments:
+        async for comment in comments:
             comment_list.append(comment.body)
             key = comment.subreddit.display_name
             top_subreddits[key] = top_subreddits[key] + 1 if key in top_subreddits else 1
 
         # Get top subreddits commented on
+        top_subreddits = {k: v for k, v in sorted(top_subreddits.items(), key=lambda x: x[1], reverse=True)}
         payload['top_subreddits'] = top_subreddits
 
         # Get top words sorted in ascending order
