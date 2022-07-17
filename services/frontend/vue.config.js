5484d8ac260dc9c3f93d0def84ba90b9f0818ef7
FS75 <juleusseah@gmail.com>
2022-07-17 20:20:49 +0800

    Update vue.config.js

--- services/frontend/vue.config.js
+++ services/frontend/vue.config.js
@@ -2,6 +2,5 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = {
   devServer: {
     allowedHosts: "all",
-    proxy: "https://reddit-crawler-backend.herokuapp.com/",
   },
 }
