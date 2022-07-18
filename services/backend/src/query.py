6be3cd03b011c60a7fa868e0e6162f0d136354b0
Dylan <dsja612@gmail.com>
2022-07-18 10:39:36 +0800

    Optimized runtime of queries

--- services/backend/src/query.py
+++ services/backend/src/query.py
@@ -2,19 +2,15 @@
 This script executes API calls and constructs data for the front-end.
 """
 
-from datetime import datetime
-from string import punctuation
-
+import time
 from decouple import config
-import json
 import asyncpraw
 from . import api
 from . import preprocess
-import praw
 from decouple import config
 from fastapi import HTTPException
 
-async def main(username: str): 
+async def main(username: str) -> dict: 
 
     reddit = asyncpraw.Reddit(client_id = config('CLIENT_ID'),
                      client_secret = config('CLIENT_SECRET'),
@@ -23,18 +19,11 @@ async def main(username: str):
     payload = {}
     
     try:
-        # Get general information
-        payload['basic_data'] = await api.getBasicData(username)
-
-        # Get no. of comments
-        payload['num_comments'] = await api.getNumSubmissions(username, 'comment')
-
-        # Get no. of submissions
-        payload['num_submissions'] = await api.getNumSubmissions(username, 'submission')
-
-        ### PRAW
+        ### ASYNCPRAW
+        start = time.time()
+        print("Starting PRAW query for /u/{}".format(username))
         user = await reddit.redditor(username)
-        comments = user.comments.new(limit=500)
+        comments = user.comments.new(limit=int(config('COMMENTS_LIMIT')))
 
         # Get body of comments
         comment_list = []
@@ -44,12 +33,25 @@ async def main(username: str):
             key = comment.subreddit.display_name
             top_subreddits[key] = top_subreddits[key] + 1 if key in top_subreddits else 1
 
+        end = time.time()
+        print("PRAW query finished in {} seconds".format(end - start))
+
+        await reddit.close()
         # Get top subreddits commented on
         top_subreddits = {k: v for k, v in sorted(top_subreddits.items(), key=lambda x: x[1], reverse=True)}
         payload['top_subreddits'] = top_subreddits
 
+        ### Other API
         # Get top words sorted in ascending order
         payload['top_words'] = await preprocess.preprocess_comments(comment_list)
+        # Get general information
+        payload['basic_data'] = await api.getBasicData(username)
+
+        # Get no. of comments
+        payload['num_comments'] = await api.getNumSubmissions(username, 'comment')
+
+        # Get no. of submissions
+        payload['num_submissions'] = await api.getNumSubmissions(username, 'submission')
 
         return payload
 
