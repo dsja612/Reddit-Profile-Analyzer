b0fc38427dfd92eb5da8561c76458f07b4bbced0
Dylan <dsja612@gmail.com>
2022-07-19 10:47:34 +0800

    Added error handling for suspended accounts

--- services/backend/src/api.py
+++ services/backend/src/api.py
@@ -9,17 +9,21 @@ from decouple import config
 import time
 
 
-async def getBasicData(username: str):
+async def getBasicData(username: str) -> dict:
     start = time.time()
     print("getBasicData start")
     userData = json.loads(requests.get('https://www.reddit.com/user/{}/about.json'.format(username), headers={'User-agent': config('USER_AGENT')}).content)
     if "error" in userData:
         raise HTTPException(status_code=404, detail='User not found!')
+    elif "message" in userData:
+        raise HTTPException(status_code=429, detail='Too many requests are sent to Reddit!')
+    elif "is_suspended" in userData["data"]:
+        raise HTTPException(status_code=404, detail='User is suspended!')
     else:
         print("getBasicData ended at {}".format(time.time() - start))
         return userData
 
-async def getNumSubmissions(username: str, type: str):
+async def getNumSubmissions(username: str, type: str) -> int:
     start = time.time()
     print("getNumSubmissions start")
     submissionsData = json.loads(requests.get('https://api.pushshift.io/reddit/search/{}/?author={}&metadata=true&size=0'.format(type, username)).content)
