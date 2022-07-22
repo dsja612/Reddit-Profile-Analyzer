8d6d8ca707e942d04481544496b4b820e112be6f
Dylan <dsja612@gmail.com>
2022-07-23 00:25:41 +0800

    Added some PrimeVue components

--- services/frontend/src/main.js
+++ services/frontend/src/main.js
@@ -6,16 +6,20 @@ import 'sweetalert2/dist/sweetalert2.min.css';
 
 import  { reactive } from 'vue'
 
+import 'primevue/resources/themes/bootstrap4-light-blue/theme.css';   //theme
+import 'primevue/resources/primevue.min.css';                   //core css
+import 'primeicons/primeicons.css';                         //icons
 import PrimeVue from 'primevue/config';
 import Badge from 'primevue/badge';
+import Button from 'primevue/button';
+import Divider from 'primevue/divider';
+import InputText from 'primevue/inputtext';
 import ProgressBar from 'primevue/progressbar';
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
 import ColumnGroup from 'primevue/columngroup';             //optional for column grouping
 import Row from 'primevue/row';                             //optional for row
-import 'primevue/resources/themes/vela-orange/theme.css';   //theme
-import 'primevue/resources/primevue.css';                   //core css
-import 'primeicons/primeicons.css';                         //icons
+
 
 export const store = reactive({
 
@@ -47,7 +51,8 @@ app.use(PrimeVue, {
         overlay: 1000,      //dropdown, overlaypanel
         menu: 1000,         //overlay menus
         tooltip: 1100       //tooltip
-    }
+    },
+    ripple: true
 });
 
 app.component(DataTable, 'DataTable')
@@ -56,5 +61,8 @@ app.component(ColumnGroup, 'ColumnGroup')
 app.component(Row, 'Row')
 app.component(Badge, 'Badge')
 app.component(ProgressBar, 'ProgressBar')
+app.component(InputText, 'InputText')
+app.component(Button, 'Button')
+app.component(Divider, 'Divider')
 
 app.mount('#app');
